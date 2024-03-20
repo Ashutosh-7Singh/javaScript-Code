a1d14548008f653a632dac790a3858fbbc0272f8
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-03-20 19:05:56 +0000

    Array1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -36,3 +36,11 @@ const myArr=[0,1,2,3,4,5]
 console.log("A",myArr);
 const myn1=myArr.slice(1,3)
 console.log(myn1);
+console.log('B',myArr);
+
+
+
+const myn2=myArr.splice(1,3)
+console.log('c',myArr);
+
+console.log(myn2);
