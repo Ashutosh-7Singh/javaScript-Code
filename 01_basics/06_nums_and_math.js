e3cda0cc9ab819442745af43871bf596272327a8
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-03-20 09:53:27 +0000

    math and number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,46 @@
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));
+
+// const otherNumber =23.8966;
+// console.log(otherNumber.toPrecision());
+// const otherNumber2=123.8966
+// console.log(otherNumber2.toPrecision(2));
+
+// const hundreds=10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));//for indian number system
+
+// ++++++++++++Math++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));//abs=absolute value means it gives always positive value
+// console.log(Math.abs(4));//abs=absolute value means it gives always positive value
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));//ceil means it give agr jra sa bhi 4 se jyada hua to next numebr to sabhi ke liye whi
+// console.log(Math.floor(4.9));//floor it give the lowee 
+
+// console.log(Math.min(2,3,4,5,6,7,8));
+// console.log(Math.max(2,3,4,5,6,7,8));
+// console.log(Math.max(2,3,4,5,6,7,8));
+
+console.log(Math.random());//it give value always between 0 to 1
+console.log((Math.random()*10)+1); //value devimal me hoga to 10 se multiply lekin agr kabhi 0 hi aaya to total value zeo ho jayega to usme plu 1
+console.log(Math.floor(Math.random()*10)+1);//value hmesa choti lane ke liye math.floor se rapup kr do baby
+
+const min =10;
+const max =20;
+console.log(
+Math.floor((Math.random()*(max-min+1))+min)
+
+
+);
+
