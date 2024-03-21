f11ad423313e0e7f89d3f3e44d87c4d695b495c8
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-03-21 18:58:17 +0000

    scope

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,27 @@
+// let a=10;
+// var b=20;
+// const c=30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//{} this is called scope when it is comes user function or if else  in json it is called object
+// if (true) {
+//     let a = 10;
+//     var b = 20;
+//     const c = 30;
+
+// }
+// // console.log(a);
+// console.log(b);
+// // console.log(c);
+
+let a=300
+if(true) {
+    let a = 10;
+    var b = 20;
+console.log("INNER:",a);
+}
+console.log(a);
+// console.log(b);
