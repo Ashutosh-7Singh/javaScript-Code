0d39494c8545b4ee945e548ce068ff7a7d1c6b39
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-04-10 07:14:38 +0000

    switch case  is complited

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,72 @@
+{/*
+// this is the syntax for switch how we can write the switch case
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/}
+
+{
+    /* 
+    const month = 3;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("february");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+
+    default:
+        console.log("defaualt case match")
+        break
+}
+    */
+}
+
+const month = "january"
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+
+    case "february":
+        console.log("february");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+
+    default:
+        console.log("defaualt case match")
+        break
+}
