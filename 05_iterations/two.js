3fa817b635934070e49c8cd739d0bd5f460b6194
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-04-10 12:24:09 +0000

    while loop complted

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index=0;
+//  while(index <= 10 ){
+//     console.log(`Value of index is ${index}`);
+//     index=index+2;
+//  }
+
+
+// let myArray=['batman','superman','flash']
+// let arr=0;
+// while(arr <myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr=arr+1;
+// }
+
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score=score+2
+} while (score<=10);
