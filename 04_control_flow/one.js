01891266471119d36686095756b1ec026d74198f
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-04-10 06:16:27 +0000

    if scope

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,1 +1,83 @@
 // if statement
+// if ke ander me true condtition hoga to execute hoga if block agr  condition false ho gya tb if ke andar ka condition execute nhi hoga
+{/* 
+if(true){
+//code execute
+}
+if(false){
+//code not exectue
+}
+*/}
+
+// some comparision operator
+{/* 
+
+<
+>
+
+<=
+
+= assiging the value 
+
+== checking the value  or data only not the type baraber hai ki nahi 
+
+!= not euqal
+
+=== it check the type  of data and data both are equal or not so tripple equal is little bit stricter 
+*/}
+const isUserLoggedIn = true
+{/*
+
+if(2=="2"){
+console.log("executed");//executed
+}
+*/}
+{/* 
+if(2==="2"){
+console.log("executed");//nothing output 
+}
+*/}
+
+{/*
+const temp=41
+
+if(temp <50){
+    console.log("temp is less than 50");
+}
+console.log("temp is greater than 50");
+
+here we got both console because  temp is less than 50 is in the if contdition so it is printed but  this console  {temp is greater than 50}doesn't have nay condiiotn so it is also printed to stop the console printing directly we have one more control folow else
+*/}
+
+{/* 
+const temp = 41
+
+if (temp < 50) {
+    console.log("temp is less than 50");
+} else {
+
+    console.log("temp is greater than 50");
+
+}
+console.log("ye to nahar me codtion ke to ye hmesa hi prinbt hoga baby ");
+
+*/}
+{/* scoper in java script  */}
+{/*
+const score =300
+
+if(score>100){
+    const power = "fly"
+    console.log(`User power:${power}`);
+} here 
+*/}
+
+{/* 
+// in this example we got an error ReferenceError: power is not defined beacaue one log is out side {} so these curly bracket define the scope also 
+const score =300
+
+if(score>100){
+    const power = "fly"
+    console.log(`User power:${power}`);
+} 
+console.log(`User power:${power}`);*/}
