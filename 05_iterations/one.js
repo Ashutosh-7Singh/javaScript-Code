eca7a4ecb777badd8548801370d60eac385ddea6
Ashutosh-7Singh <149756205+Ashutosh-7Singh@users.noreply.github.com>
2024-04-10 11:35:32 +0000

    array in loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,30 @@
+// for loop 
+// for( let  i=0 ;i<= 10; i++){
+//     const element=i;
+//     console.log(element);
+// }
+
+// for( let  i=0 ;i<= 10; i++){
+//     const element=i;
+//     if(element== 5){
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+
+
+// for( let  i=0 ;i<= 10; i++){
+//    console.log(`Outer loop value :${i}`)
+//     for(let j=0 ;j<=10;j++){
+// // console.log(`Inner loop value ${j} and Inner loop ${i}`);
+// console.log(i+'*' +j +'='+i*j)
+//    }
+// }
+
+let myArray=["flash","batman","superman"]
+for(let  i=0;i<myArray.lengthl;i++){
+    const element=myArray[i]
+    console.log(element);
+
+}
